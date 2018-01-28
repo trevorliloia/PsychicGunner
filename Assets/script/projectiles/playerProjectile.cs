2240c83a2320e6293d254efced682d5e3592ed20
Shawn Hubbart <shawn.hubbart@gmail.com>
2018-01-28 15:23:28 -0800

    Added player projectile

---
+++ Assets/script/projectiles/playerProjectile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerProjectile : MonoBehaviour
+{
+    [SerializeField] float damage, duration, speed;
+    float durationReset;
+
+    // Use this for initialization
+    void Awake()
+    {
+        durationReset = duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        duration -= Time.deltaTime;
+        if (duration <= 0)
+        {
+            duration = durationReset;
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Movement()
+    {
+        transform.position += (transform.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.GetComponent<enemyLandBasic>().TakeDamage(damage);
+            duration = durationReset;
+            gameObject.SetActive(false);
+        }
+    }
+}
+
