2d3017a71cdff115223f237a257bf7bd125c5ba4
Shawn Hubbart <shawn.hubbart@gmail.com>
2018-01-27 17:33:09 -0800

    Implemented object pool, player damage, and projectile spawning

--- Assets/script/doShoot.cs
+++ Assets/script/doShoot.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class doShoot : MonoBehaviour
 {
+    public objectPool objectPool;
+    public Transform spawnLocation;
+    public pooledObjectKey pooledObject;
 
     public void FireWeapon()
     {
         Debug.Log("Pew Pew");
+        GameObject bullet = objectPool.sharedInstance.GetPooledObject(pooledObject);
+        if (bullet != null)
+        {
+            bullet.transform.position = spawnLocation.position;
+            bullet.transform.rotation = spawnLocation.rotation;
+            bullet.SetActive(true);
+        }
     }
 
 }
