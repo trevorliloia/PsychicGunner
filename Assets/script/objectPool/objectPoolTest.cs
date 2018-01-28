d6d37e31eb3424939a90b32a5d89bdab3a03ca84
trevorliloia <trevorliloia@gmail.com>
2018-01-28 14:49:22 -0800

    facility level

--- Assets/script/objectPool/objectPoolTest.cs
+++ Assets/script/objectPool/objectPoolTest.cs
@@ -17,7 +17,7 @@ public class objectPoolTest : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.A)) {
-            GameObject obj = pool.GetPooledObject(pooledObjectKey.BULLET_CLASSIC);
+            GameObject obj = pool.GetPooledObject(pooledObjectKey.BULLET_1);
             if (obj != null) {
                 obj.SetActive(true);
                 activeObj.Add(obj);
