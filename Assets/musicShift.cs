6a554e0e25569d20e0f46b5cec34eea83cbb28fe
trevorliloia <trevorliloia@gmail.com>
2018-01-27 18:56:20 -0800

    sound shifting

---
+++ Assets/musicShift.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class musicShift : MonoBehaviour {
+
+    public bool action;
+    public AudioSource bg;
+    public AudioSource ac;
+	void Start () {
+        action = false;
+        bg.volume = 1;
+        ac.volume = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(action)
+        {
+            ac.volume += Time.deltaTime;
+            bg.volume -= Time.deltaTime;
+        }
+        else
+        {
+            ac.volume -= Time.deltaTime;
+            bg.volume += Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            action = !action;
+        }
+	}
+}
