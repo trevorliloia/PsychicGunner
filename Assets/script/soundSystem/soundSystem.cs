bfd3b07d86fdd1279cc483ecea81b964d7d66f2a
Yuchen Huang <oocasthy@umich.edu>
2018-01-27 18:48:41 -0800

    Add sound system and test

---
+++ Assets/script/soundSystem/soundSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundSystem : MonoBehaviour {
+    public static soundSystem sharedInstance;
+
+    public List<audioClipMeta> audioClipMetas;
+    private Dictionary<audioClipKey, audioClipMeta> audioClipMetaMap;
+
+    void Awake() {
+        if (sharedInstance == null) {
+            sharedInstance = this;
+        }
+
+        audioClipMetaMap = new Dictionary<audioClipKey, audioClipMeta>();
+
+        foreach (audioClipMeta meta in audioClipMetas) {
+            audioClipMetaMap[meta.key] = meta;
+        }
+        // TODO: load and cache audio clips dynamically
+    }
+
+    public void Play(audioClipKey key) {
+        GameObject audioSourceObj = objectPool.sharedInstance.GetPooledObject(pooledObjectKey.AUDIO_SOURCE);
+        if (audioSourceObj != null) {
+            audioSourceObj.SetActive(true);
+            AudioSource audioSource = audioSourceObj.GetComponent<AudioSource>();
+            audioSource.clip = audioClipMetaMap[key].clip;
+            audioSource.Play();
+            StartCoroutine(ClipTimer(audioSourceObj));
+        }
+    }
+
+    private IEnumerator ClipTimer(GameObject audioSourceObj) {
+        yield return new WaitWhile(() => audioSourceObj.GetComponent<AudioSource>().isPlaying);
+
+        audioSourceObj.SetActive(false);
+    }
+}
---
+++ Assets/script/soundSystem/soundSystemTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundSystemTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+            soundSystem.sharedInstance.Play(audioClipKey.TEST_CLIP_1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            soundSystem.sharedInstance.Play(audioClipKey.TEST_CLIP_2);
+        }
+	}
+}
